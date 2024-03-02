2c96483c0775fceebb319d5cc57f40b972e09afa
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-02 14:23:17 -0300

    Adicionado configuração para criar o banco de dados e configuração para DI.

--- src/JobsFinder.Api/Program.cs
+++ src/JobsFinder.Api/Program.cs
@@ -1,3 +1,8 @@
+using JobsFinder.Domain.Extension;
+using JobsFinder.Infrastructure.Database;
+using JobsFinder.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<JobsFinderContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConexaoCompleta())
+    );
+
+builder.Services.AddRepositorio(builder.Configuration);
 
 var app = builder.Build();
 
@@ -22,4 +33,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+AtualizarBaseDeDados();
+
 app.Run();
+
+void AtualizarBaseDeDados()
+{
+    var conexao = builder.Configuration.GetConexao();
+    var nomeDatabase = builder.Configuration.GetNomeDatabase();
+
+    Database.CreateDatabase(conexao, nomeDatabase);
+}
---
+++ src/JobsFinder.Infrastructure/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using JobsFinder.Domain.Extension;
+using JobsFinder.Domain.Repository;
+using JobsFinder.Domain.Repository.InterfaceCidade;
+using JobsFinder.Infrastructure.Database;
+using JobsFinder.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobsFinder.Infrastructure;
+public static class Bootstrapper
+{
+    public static void AddRepositorio(this IServiceCollection services,
+        IConfiguration configurationManager)
+    {
+        AddContext(services, configurationManager);
+        AddUnitOfWord(services);
+        AddRepositorios(services);
+    } 
+
+    private static void AddRepositorios(IServiceCollection services)
+    {
+        services.AddScoped<ICidadeWriteOnlyRepository, CidadeRepository>()
+                .AddScoped<ICidadeReadOnlyRepository, CidadeRepository>();
+    }
+
+    private static void AddContext(IServiceCollection services, IConfiguration configurationManager)
+    {
+        var conexao = configurationManager.GetConexaoCompleta();
+
+        services.AddDbContext<JobsFinderContext>(dbContextoOpcoes =>
+        {
+            dbContextoOpcoes.UseNpgsql(conexao);
+        });
+    }
+
+    private static void AddUnitOfWord(IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
