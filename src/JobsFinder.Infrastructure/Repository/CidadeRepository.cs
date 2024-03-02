a6efbf40914b1c8dbfd44bff0133b7ce061e6cb3
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-02 14:19:32 -0300

    Adicionado repository de Cidade.

---
+++ src/JobsFinder.Infrastructure/Repository/CidadeRepository.cs
@@ -0,0 +1,27 @@
+﻿using JobsFinder.Domain.Entities;
+using JobsFinder.Domain.Repository.InterfaceCidade;
+using JobsFinder.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobsFinder.Infrastructure.Repository;
+public class CidadeRepository : ICidadeWriteOnlyRepository, ICidadeReadOnlyRepository
+{
+    private readonly JobsFinderContext _context;
+
+    public CidadeRepository(JobsFinderContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Adicionar(Cidade cidade)
+    {
+        await _context.Cidades.AddAsync(cidade);
+    }
+
+    public async Task<bool> ExisteCidade(string nomeCidade)
+    {
+        return await _context.Cidades.AnyAsync(
+            c => c.NomeCidade.Equals(nomeCidade)
+            );
+    }
+}
