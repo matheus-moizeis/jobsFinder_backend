2bdd844edac20a7a2998cf89f1c8cd80eff1235c
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-02 14:19:03 -0300

    Adicionado configuração do ef core.

---
+++ src/JobsFinder.Infrastructure/Database/Database.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace JobsFinder.Infrastructure.Database;
+public static class Database
+{
+    public static void CreateDatabase(string conexaoBD, string nomeBD)
+    {
+        using var connection = new NpgsqlConnection(conexaoBD);
+
+        var parametros = new DynamicParameters();
+        parametros.Add("nomeBD", nomeBD);
+
+        var registros = connection.Query("SELECT datname FROM pg_database WHERE datname = @nomeBD",
+            parametros);
+
+        if (!registros.Any()) connection.Execute($"CREATE DATABASE {nomeBD}");
+    }
+}
