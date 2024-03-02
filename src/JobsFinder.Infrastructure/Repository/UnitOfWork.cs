f9b17cadd57ae23049335a37ad07580e32a76850
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-02 14:24:17 -0300

    Adicionado padrão Unit Of Work.

---
+++ src/JobsFinder.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using JobsFinder.Domain.Repository;
+using JobsFinder.Infrastructure.Database;
+
+namespace JobsFinder.Infrastructure.Repository;
+public sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly JobsFinderContext _context;
+    private bool _disposed;
+
+    public UnitOfWork(JobsFinderContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Commit()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (!_disposed && disposing) _context.Dispose();
+
+        _disposed = true;
+    }
+}
