73978d2bbab607b37f7622142be5f916e9aa54bd
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-03 15:58:52 -0300

    feat: Adicionado migration da entidade Cidade.

---
+++ src/JobsFinder.Infrastructure/Migrations/20240303183342_Cidade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace JobsFinder.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cidades",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NomeCidade = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    CodIbge = table.Column<int>(type: "integer", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cidades", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cidades");
+        }
+    }
+}
