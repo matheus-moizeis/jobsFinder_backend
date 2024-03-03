c565d7d81872881e4f34771b764159ef87e49e61
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-03 15:59:22 -0300

    feat: Configuração do mapeamento

---
+++ src/JobsFinder.Infrastructure/Database/Mapping/BaseMap.cs
@@ -0,0 +1,22 @@
+﻿using JobsFinder.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobsFinder.Infrastructure.Database.Mapping;
+public class BaseMap<T> : IEntityTypeConfiguration<T> where T : EntityBase
+{
+    private readonly string _tableName;
+    public BaseMap(string tableName)
+    {
+
+        _tableName = tableName;
+
+    }
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        if(string.IsNullOrEmpty(_tableName)) builder.ToTable(_tableName);
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+    }
+}
---
+++ src/JobsFinder.Infrastructure/Database/Mapping/CidadeMap.cs
@@ -0,0 +1,17 @@
+﻿using JobsFinder.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobsFinder.Infrastructure.Database.Mapping;
+public class CidadeMap : BaseMap<Cidade>
+{
+    public CidadeMap() : base("Cidade")
+    {
+    }
+
+    public override void Configure(EntityTypeBuilder<Cidade> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.NomeCidade).HasMaxLength(50);
+    }
+}
