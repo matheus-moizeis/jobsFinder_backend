6795e87cbc2096a35bdaeda8d459d3ee9d6b46d1
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-03 16:08:46 -0300

    feat: Configuração comunicação da entidade Cidade.

---
+++ src/Shared/JobsFinder.Communication/Request/ReqResgitrarCidadeJson.cs
@@ -0,0 +1,7 @@
+﻿namespace JobsFinder.Communication.Request;
+public class ReqResgitrarCidadeJson
+{
+    public string NomeCidade { get; set; }
+    public int CodIbge { get; set; }
+    public long IdEstado { get; set; }
+}
---
+++ src/Shared/JobsFinder.Communication/Response/ResCidadeRegistradaJson.cs
@@ -0,0 +1,8 @@
+﻿namespace JobsFinder.Communication.Response;
+public class ResCidadeRegistradaJson
+{
+    public long Id { get; set; }
+    public string NomeCidade { get; set; }
+    public int CodIbge { get; set; }
+    public long IdEstado { get; set; }
+}
