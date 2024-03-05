8adf5a238e0657da85115f6be03ed24eea589aae
Matheus Moizeis <matheusmoizeis@outlook.com>
2024-03-04 22:31:23 -0300

    feat: Adicionado validação da requisição do usuario para criar Cidade.

---
+++ src/JobsFinder.Application/UseCase/Cidade/RegistrarCidadeValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using JobsFinder.Communication.Request;
+using JobsFinder.Exceptions;
+
+namespace JobsFinder.Application.UseCase.Cidade;
+public class RegistrarCidadeValidator : AbstractValidator<ReqResgitrarCidadeJson>
+{
+    public RegistrarCidadeValidator()
+    {
+        RuleFor(x => x.NomeCidade)
+         .NotEmpty()
+         .WithMessage(ResourceMensagensDeErro.Nome_Cidade_Vazia);
+
+        RuleFor(x => x.CodIbge)
+         .NotEmpty()
+         .WithMessage(ResourceMensagensDeErro.Ibge_Vazio);
+
+        RuleFor(x => x.IdEstado)
+         .NotEmpty()
+         .WithMessage(ResourceMensagensDeErro.Estado_Vazio);
+    }
+}
